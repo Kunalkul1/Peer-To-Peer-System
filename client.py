ead17fd81fb09ab8ddcf5accd4cfa443cbf87d97
kunalkul1 <kunalkul1@gmail.com>
2018-03-26 18:35:37 -0400

    Added menu and adding rfc method

--- client.py
+++ client.py
@@ -1,8 +1,13 @@
 from socket import *
 import time
+import threading
 PORT = 7734
 
 class clientSocket:
+	def __init__(self, port):
+		threading.Thread.__init__(self)
+		self.port = port
+		self.host = gethostname()
 
 	#Function to create client socket
 	def create_socket(self):
@@ -30,6 +35,43 @@ class clientSocket:
 
 	def close(self):
 		self.sock.close()
+
+	def main_menu(self):
+		while(1):
+			choice = int(input("Choose an option: \n 1. ADD RFC \n 2. LOOKUP RFC \n 3. LIST RFCs \n 4. DOWNLOAD RFC \n 5. Exit \n"))
+			if choice == 1:
+				self.add_RFC()
+			if choice == 2:
+				self.lookup_RFC()
+			if choice == 3:
+				self.list_RFC()
+			if choice == 4:
+				self.download_RFC()
+			if choice == 5:
+				self.exit()
+				break
+			else:
+				print "Invalid option. Please enter again!\n"
+
+	def add_RFC(self):
+		RFC_number = int(input("Enter the RFC number \n"))
+		RFC_filename = "RFC " + str(RFC_number) + ".txt"
+		RFC_title = raw_input("Enter RFC title")
+		if RFC_filename in os.listdir("."):
+			peer2server_message = self.create_peer2server_message("ADD", self.host, self.port, RFC_number, RFC_title)
+
+	def create_peer2server_message(self, request_type, host, port, RFC_number, RFC_title=""):
+		message = ""
+		message = message + request_type + " "
+		if RFC_number != 0:
+			message = message + "RFC " + str(RFC_number) + " "
+		message = message + "Host: " + host + "\r\n"
+		message = message + "Port: " + str(port)
+
+		if RFC_title:
+			message = message + "\r\n" + "Title: " + RFC_title
+		print message
+		return message
 
 def main():
 	sobj = clientSocket()
