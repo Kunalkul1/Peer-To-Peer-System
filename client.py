142139a7acf1a664c4d6dd118954daee061d5e02
kunalkul1 <kunalkul1@gmail.com>
2018-03-27 14:34:43 -0400

    Added lookup working

--- client.py
+++ client.py
@@ -4,94 +4,102 @@ import os
 BUFFERSIZE = 1024000
 
 class clientSocket(threading.Thread):
-	def __init__(self, port):
-		threading.Thread.__init__(self)
-		self.port = port
-		self.host = gethostname()
+    def __init__(self, port):
+        threading.Thread.__init__(self)
+        self.port = port
+        self.host = gethostname()
 
-	def run(self):
-		self.create_socket()
-		self.start_connections('localhost',7734)
-		print "hii"
-		self.show_main_menu()
+    def run(self):
+        self.create_socket()
+        self.start_connections('localhost', 7734)
+        self.show_main_menu()
 
-	#Function to create client socket
-	def create_socket(self):
-		self.sock = socket(AF_INET,SOCK_STREAM)
+    #Function to create client socket
+    def create_socket(self):
+        self.sock = socket(AF_INET,SOCK_STREAM)
 
-	#Function to connect
-	def start_connections(self,serverName,port):
-		self.sock.connect((serverName,port))
+    #Function to connect
+    def start_connections(self,serverName,port):
+        self.sock.connect((serverName,port))
 
-	def send(self,str):
-		#print str
-		self.sock.send(str)
-		#print "sent"
+    def send(self,str):
+        #print str
+        self.sock.send(str)
+        #print "sent"
 
-	def recv(self):
-		try:
-			#time.sleep(0.1)
-			s = self.sock.recv(1024)
-			return s
-		except:
-			print "recv failed"
+    def recv(self):
+        try:
+            #time.sleep(0.1)
+            s = self.sock.recv(1024)
+            return s
+        except:
+            print "recv failed"
 
-	def close(self):
-		self.sock.close()
+    def close(self):
+        self.sock.close()
 
-	def show_main_menu(self):
-		while True:
-			choice = int(input("Choose an option: \n 1. ADD RFC \n 2. LOOKUP RFC \n 3. LIST RFCs \n 4. DOWNLOAD RFC \n 5. Exit \n"))
-			if choice == 1:
-				self.add_RFC()
-			if choice == 2:
-				self.lookup_RFC()
-			if choice == 3:
-				self.list_RFC()
-			if choice == 4:
-				self.download_RFC()
-			if choice == 5:
-				self.exit()
-				break
-			else:
-				print "Invalid option. Please enter again!\n"
+    def show_main_menu(self):
+        while True:
+            choice = int(input("Choose an option: \n 1. ADD RFC \n 2. LOOKUP RFC \n 3. LIST RFCs \n 4. DOWNLOAD RFC \n 5. Exit \n"))
+            if choice == 1:
+                self.add_RFC()
+            if choice == 2:
+                self.lookup_RFC()
+            if choice == 3:
+                self.list_RFC()
+            if choice == 4:
+                self.download_RFC()
+            if choice == 5:
+                self.exit()
+                break
+            else:
+                print "Invalid option. Please enter again!\n"
 
-	def add_RFC(self):
-		RFC_number = int(input("Enter the RFC number \n"))
-		RFC_filename = "RFC " + str(RFC_number) + ".txt"
-		RFC_title = raw_input("Enter RFC title \n")
-		if RFC_filename in os.listdir("./RFC"):
-			peer2server_message = self.create_peer2server_message("ADD", self.host, self.port, RFC_number, RFC_title)
-			self.sock.send(peer2server_message)
-			payload = self.sock.recv(BUFFERSIZE)
-			print payload
+    def add_RFC(self):
+        RFC_number = int(input("Enter the RFC number \n"))
+        RFC_filename = "RFC " + str(RFC_number) + ".txt"
+        RFC_title = raw_input("Enter RFC title \n")
+        if RFC_filename in os.listdir("./RFC"):
+            peer2server_message = self.create_peer2server_message("ADD", self.host, self.port, RFC_number, RFC_title)
+            self.sock.send(peer2server_message)
+            payload = self.sock.recv(BUFFERSIZE)
+            print payload
 
-		else:
-			print("RFC is not present in current directory. \n")
+        else:
+            print("RFC is not present in RFC directory. \n")
 
-	def create_peer2server_message(self, request_type, host, port, RFC_number, RFC_title=""):
-		message = ""
-		message = message + request_type + " "
-		if RFC_number != 0:
-			message = message + "RFC " + str(RFC_number) + " "
-		message = message + "P2P-CI/1.0" + "\r\n"
-		message = message + "Host: " + host + "\r\n"
-		message = message + "Port: " + str(port)
+    def create_peer2server_message(self, request_type, host, port, RFC_number, RFC_title=""):
+        message = ""
+        message = message + request_type + " "
+        if RFC_number != 0:
+            message = message + "RFC " + str(RFC_number) + " "
+        message = message + "P2P-CI/1.0" + "\r\n"
+        message = message + "Host: " + host + "\r\n"
+        message = message + "Port: " + str(port)
 
-		if RFC_title:
-			message = message + "\r\n" + "Title: " + RFC_title
-		print message
-		return message
+        if RFC_title:
+            message = message + "\r\n" + "Title: " + RFC_title
+        print "The message looks like follows:\n" + message
+        return message
 
-	def exit(self):
-		print "Exiting!\n"
-		self.sock.close()
-		return
+    def lookup_RFC(self):
+        RFC_number = int(input("Enter the RFC number to be looked up\n"))
+        RFC_filename = raw_input("Enter the RFC title too\n")
+        message = self.create_peer2server_message("LOOKUP", self.host, self.port, RFC_number, RFC_filename)
+        self.sock.send(message)
+        print "Message sent!"
+        payload = self.sock.recv(BUFFERSIZE)
+        print "Payload is: " + payload
+
+    def exit(self):
+        print "Exiting!\n"
+        self.sock.close()
+        return
 
 def main():
-	upload_port = input("Enter the upload port number of this client:\n")
-	sobj = clientSocket(upload_port)
-	sobj.start()
+    upload_port = input("Enter the upload port number of this client:\n")
+    sobj = clientSocket(upload_port)
+    sobj.start()
 
 if __name__ == "__main__":
-	main()
+    main()
--- server.py
+++ server.py
@@ -7,138 +7,156 @@ peerlist = []
 list_of_rfcs = []
 
 class serverSocket(threading.Thread):
-	def __init__(self, peerinfo):
-		threading.Thread.__init__(self)
-		self.sockfd = peerinfo[0]
-		self.address = peerinfo[1]
+    def __init__(self, peerinfo):
+        threading.Thread.__init__(self)
+        self.sockfd = peerinfo[0]
+        self.address = peerinfo[1]
 
-	def run(self):
-		while True:
-			payload = self.sockfd.recv(BUFFERSIZE)
-			if payload.startswith("EXIT"):
-				break
-			if not payload:
-				break
-			self.process_request(payload)
+    def run(self):
+        while True:
+            payload = self.sockfd.recv(BUFFERSIZE)
+            if payload.startswith("EXIT"):
+                break
+            if not payload:
+                break
+            self.process_request(payload)
 
-	#Function to create socket
-	def create_socket(self):
-		try:
-			self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-			self.sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-			self.sock.bind(('',PORT))
-			self.sock.listen(10)
-		except:
-			print "Socket creation failed"
+    #Function to create socket
+    def create_socket(self):
+        try:
+            self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            self.sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+            self.sock.bind(('',PORT))
+            self.sock.listen(10)
+        except:
+            print "Socket creation failed"
 
-	#Function to accept connections
-	def accept_connections(self):
-		try:
-			(self.csock, self.caddr) = self.sock.accept()
+    #Function to accept connections
+    def accept_connections(self):
+        try:
+            (self.csock, self.caddr) = self.sock.accept()
 
-		except:
-			print "accept_connection failed!"
-			self.closeServerSocketFromChild()
-			os.system('kill %d' % os.getpid())
+        except:
+            print "accept_connection failed!"
+            self.closeServerSocketFromChild()
+            os.system('kill %d' % os.getpid())
 
-	def process_request(self, request):
-		self.validate_request(request)
-		if self.status_code == 200:
-			split_request = request.split("\r\n")
-			if not (split_request[1].lstrip('Host: '), split_request[2].lstrip('Port: ')) in peerlist:
-				peerlist.append((split_request[1].lstrip('Host: '), split_request[2].lstrip('Port: ')))
-				print "Appended to Peer List!\n"
-			if request.startswith('ADD'):
-				self.add_rfc(request)
-			self.send_response(request)
-		else:
-			response = "P2P-CI/1.0 " + str(self.status_code) + " " + self.phrase
-			self.sockfd.send(response)
-		print "Peer list: " + str(peerlist)
+    def process_request(self, request):
+        self.validate_request(request)
+        if self.status_code == 200:
+            split_request = request.split("\r\n")
+            if not (split_request[1].lstrip('Host: '), split_request[2].lstrip('Port: ')) in peerlist:
+                peerlist.append((split_request[1].lstrip('Host: '), split_request[2].lstrip('Port: ')))
+                print "Appended to Peer List!\n"
+            if request.startswith('ADD'):
+                self.add_rfc(request)
+            self.send_response(request)
+        else:
+            response = "P2P-CI/1.0 " + str(self.status_code) + " " + self.phrase
+            self.sockfd.send(response)
+        print "Peer list: " + str(peerlist)
 
-	def validate_request(self, request):
-		self.status_code = 0
-		self.phrase = ""
-		split_request = request.split("\r\n")
-		if "P2P-CI/1.0" not in split_request[0]:
-			self.status_code = 505
-			self.phrase = "P2P-CI Version Not Supported"
+    def validate_request(self, request):
+        self.status_code = 0
+        self.phrase = ""
+        split_request = request.split("\r\n")
+        if "P2P-CI/1.0" not in split_request[0]:
+            self.status_code = 505
+            self.phrase = "P2P-CI Version Not Supported"
 
-		elif not ((split_request[0].startswith("ADD") or split_request[0].startswith("LOOKUP") or split_request[0].startswith("LIST")) and (split_request[1].startswith("Host: ") and split_request[2].startswith("Port: "))):
-			self.status_code = 400
-			self.phrase = "Bad Request"
+        elif not ((split_request[0].startswith("ADD") or split_request[0].startswith("LOOKUP") or split_request[0].startswith("LIST")) and (split_request[1].startswith("Host: ") and split_request[2].startswith("Port: "))):
+            self.status_code = 400
+            self.phrase = "Bad Request"
 
-		elif not split_request[0].startswith("LIST"):
-			if not split_request[len(split_request) - 1].startswith("Title: "):
-				self.status_code = 400
-				self.phrase = "Bad Request"
-			else:
-				self.status_code = 200
-				self.phrase = "OK"
+        elif not split_request[0].startswith("LIST"):
+            if not split_request[len(split_request) - 1].startswith("Title: "):
+                self.status_code = 400
+                self.phrase = "Bad Request"
+            else:
+                self.status_code = 200
+                self.phrase = "OK"
 
-		else:
-			self.status_code = 200
-			self.phrase = "OK"
+        else:
+            self.status_code = 200
+            self.phrase = "OK"
 
-	def add_rfc(self, request):
-		split_request = request.split('\r\n')
-		if (split_request[0].lstrip("ADD RFC ").replace(' P2P-CI/1.0', ''), split_request[len(split_request) - 1].lstrip("Title: "),
-			split_request[1].lstrip("Host: ")) not in list_of_rfcs:
-			list_of_rfcs.append((split_request[0].lstrip("ADD RFC ").replace(' P2P-CI/1.0', ''),
-								 split_request[len(split_request) - 1].lstrip("Title: "), split_request[1].lstrip("Host: "),
-								 split_request[2].lstrip("Port: ")))
-			print "List of RFCs has been updated!\n"
-		print "List of RFCs: " + str(list_of_rfcs)
+    def add_rfc(self, request):
+        split_request = request.split('\r\n')
+        if (split_request[0].lstrip("ADD RFC ").replace(' P2P-CI/1.0', ''), split_request[len(split_request) - 1].lstrip("Title: "),
+            split_request[1].lstrip("Host: ")) not in list_of_rfcs:
+            list_of_rfcs.append((split_request[0].lstrip("ADD RFC ").replace(' P2P-CI/1.0', ''),
+                                 split_request[len(split_request) - 1].lstrip("Title: "), split_request[1].lstrip("Host: ")))
+            print "List of RFCs has been updated!\n"
+        print "List of RFCs: " + str(list_of_rfcs)
 
-	def send_response(self, request):
-		response = ""
-		split_request = request.split('\r\n')
-		if split_request[0].startswith("ADD"):
-			response = response + "P2P-CI/1.0 " + str(self.status_code) + " " + self.phrase
-			response = response + "\r\n" + split_request[0].lstrip("ADD ").rstrip("P2P-CI/1.0")
-			response = response + split_request[len(split_request) - 1].lstrip("Title: ")
-			response = response + split_request[1].lstrip("Host:") + split_request[2].lstrip("Port:")
+    def lookup_RFC(self, split_request):
+        rfclist = []
+        for(rfc, title, host) in list_of_rfcs:
+            if (rfc == split_request[0].lstrip("LOOKUP RFC ").replace(' P2P-CI/1.0', '') and (title == split_request[len(split_request) - 1].lstrip("Title: "))):
+                rfclist.append((rfc, title, host))
+        return rfclist
 
-		self.sockfd.send(bytes(response))
+    def send_response(self, request):
+        response = ""
+        split_request = request.split('\r\n')
+        if split_request[0].startswith("ADD"):
+            response = response + "P2P-CI/1.0 " + str(self.status_code) + " " + self.phrase
+            response = response + "\r\n" + split_request[0].lstrip("ADD ").rstrip("P2P-CI/1.0")
+            response = response + split_request[len(split_request) - 1].lstrip("Title: ")
+            response = response + split_request[1].lstrip("Host:") + split_request[2].lstrip("Port:")
 
-	#Function to close the Client connection from Parent process
-	def closeClientSocketFromParent(self):
-		self.csock.close()
+        elif(split_request[0].startswith("LOOKUP")):
+            lookup_results = self.lookup_RFC(split_request)
+            if len(lookup_results) == 0:
+                self.status_code = 404
+                self.phrase = "Not Found"
+                response = response + "P2P-CI/1.0 " + str(self.status_code) + " " + self.phrase
+            else:
+                response = response + "P2P-CI/1.0 " + str(self.status_code) + " " + self.phrase
+                for(rfc, title, host) in lookup_results:
+                    response = response + "\r\n" + "RFC " + rfc + " " + title + " " + host
+                print response
 
-	#Function to close the Server connection from the child process
-	def closeServerSocketFromChild(self):
-		self.sock.close()
+        self.sockfd.send(bytes(response))
+
+    #Function to close the Client connection from Parent process
+    def closeClientSocketFromParent(self):
+        self.csock.close()
+
+    #Function to close the Server connection from the child process
+    def closeServerSocketFromChild(self):
+        self.sock.close()
 
 
 #This class has all the functions performed by the Slave socket
 class Child:
-	def __init__(self,sock,addr):
-		self.sock = sock
-		self.addr = addr
+    def __init__(self,sock,addr):
+        self.sock = sock
+        self.addr = addr
 
-	def register(self):
-		print "Client registered!"
+    def register(self):
+        print "Client registered!"
 
-	def close(self):
-		print "Closing connection from client with address ", self.addr
-		self.sock.close()
-		print "Connection closed!"
+    def close(self):
+        print "Closing connection from client with address ", self.addr
+        self.sock.close()
+        print "Connection closed!"
 
 def main():
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock.bind(('', PORT))
-	sock.listen(10)
-	print "Server is up and running!\n"
-	while True:
-		try:
-			server = serverSocket(sock.accept())
-			print "Client registered!\n"
-			server.start()
-		except:
-			print "Error in connecting/sending data to the client\n"
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('', PORT))
+    sock.listen(10)
+    print "Server is up and running!\n"
+    while True:
+        try:
+            server = serverSocket(sock.accept())
+            print "Client registered!\n"
+            server.start()
+        except:
+            print "Error in connecting/sending data to the client\n"
 
 if __name__ == "__main__":
-	main()
+    main()
 
 
 
