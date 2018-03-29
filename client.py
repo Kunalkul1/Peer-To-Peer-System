ee54ac1909033478ea37b3dad3d68487c14701f9
kunalkul1 <kunalkul1@gmail.com>
2018-03-29 15:29:28 -0400

    Changed add rfc working

--- client.py
+++ client.py
@@ -12,6 +12,8 @@ class clientSocket(threading.Thread):
     def run(self):
         self.create_socket()
         self.start_connections('localhost', 7734)
+        self.add_RFC()
+        print "Registered your RFC information on server!\n"
         self.show_main_menu()
 
     #Function to create client socket
@@ -40,33 +42,43 @@ class clientSocket(threading.Thread):
 
     def show_main_menu(self):
         while True:
-            choice = int(input("Choose an option: \n 1. ADD RFC \n 2. LOOKUP RFC \n 3. LIST RFCs \n 4. DOWNLOAD RFC \n 5. Exit \n"))
+            choice = int(input("Choose an option: \n 1. LOOKUP RFC \n 2. LIST RFCs \n 3. DOWNLOAD RFC \n 4. Exit \n"))
             if choice == 1:
-                self.add_RFC()
-            if choice == 2:
                 self.lookup_RFC()
-            if choice == 3:
+            if choice == 2:
                 self.list_RFC()
-            if choice == 4:
+            if choice == 3:
                 self.download_RFC()
-            if choice == 5:
+            if choice == 4:
                 self.exit()
                 break
             else:
                 print "Invalid option. Please enter again!\n"
 
     def add_RFC(self):
-        RFC_number = int(input("Enter the RFC number \n"))
-        RFC_filename = "RFC " + str(RFC_number) + ".txt"
-        RFC_title = raw_input("Enter RFC title \n")
-        if RFC_filename in os.listdir("./RFC"):
-            peer2server_message = self.create_peer2server_message("ADD", self.host, self.port, RFC_number, RFC_title)
-            self.sock.send(peer2server_message)
+        rfc_files = []
+        for file in os.listdir("./RFC"):
+            if file.startswith("RFC"):
+                rfc_files.append(file)
+        for file in rfc_files:
+            rfc_title = raw_input("Enter Title for RFC " + file.lstrip("RFC ").lstrip() + ":\n")
+            message = self.create_peer2server_message("ADD", self.host, self.port, file.lstrip("RFC ").lstrip().rstrip('.txt'), rfc_title)
+            self.sock.send(bytes(message))
             payload = self.sock.recv(BUFFERSIZE)
-            print payload
+            print payload + "\n"
 
-        else:
-            print("RFC is not present in RFC directory. \n")
+    # def add_RFC(self):
+    #     RFC_number = int(input("Enter the RFC number \n"))
+    #     RFC_filename = "RFC " + str(RFC_number) + ".txt"
+    #     RFC_title = raw_input("Enter RFC title \n")
+    #     if RFC_filename in os.listdir("./RFC"):
+    #         peer2server_message = self.create_peer2server_message("ADD", self.host, self.port, RFC_number, RFC_title)
+    #         self.sock.send(peer2server_message)
+    #         payload = self.sock.recv(BUFFERSIZE)
+    #         print payload
+    #
+    #     else:
+    #         print("RFC is not present in RFC directory. \n")
 
     def create_peer2server_message(self, request_type, host, port, RFC_number, RFC_title = ""):
         message = ""
@@ -79,7 +91,7 @@ class clientSocket(threading.Thread):
 
         if RFC_title:
             message = message + "\r\n" + "Title: " + RFC_title
-        print "The message that was sent:\n" + message
+        print "The message that was sent:\n" + message + "\n"
         return message
 
     def lookup_RFC(self):
@@ -88,14 +100,14 @@ class clientSocket(threading.Thread):
         message = self.create_peer2server_message("LOOKUP", self.host, self.port, RFC_number, title)
         self.sock.send(message)
         payload = self.sock.recv(BUFFERSIZE)
-        print "Peers having the specified RFC:\n" + payload
+        print "Peers having the specified RFC:\n" + payload + "\n"
 
     def list_RFC(self):
         message = self.create_peer2server_message("LIST", self.host, self.port, 0)
         self.sock.send(message)
         payload = self.sock.recv(BUFFERSIZE)
         print "List of all RFCS:\n"
-        print payload
+        print payload + "\n"
 
     def download_RFC(self):
         rfc = int(input("Enter RFC number \n"))
